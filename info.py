ff7cc6e2d8445fb4d0a64ae4a76e932d7cb812e8
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2023-10-01 01:20:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'PUsHPAFILIMFAKTORY')
-API_ID = int(environ.get('API_ID', '5917107'))
+API_ID = int(environ.get('API_ID', '15917107'))
 API_HASH = environ.get('API_HASH', '8197e7638d58c92ae2504adba7c62117')
 BOT_TOKEN = environ.get('BOT_TOKEN', "5736646811:AAHXmO-XoV5llwCm4c4q3AFaH2mGi5wCRxI")
 
