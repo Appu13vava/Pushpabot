fa957ece44a967b90dd069349ae4d796d4472c8a
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2023-09-30 21:43:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,12 +49,7 @@ single_button_env = os.environ.get('SINGLE_BUTTON', 'True')
 
 SINGLE_BUTTON = is_enabled(single_button_env)
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '--1001930977544')            
-INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
-PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
-
+                  
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
 LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
