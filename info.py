f5b8afb07b167ca3c7853e87f58ab4871430e283
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2023-09-30 21:48:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,11 +51,7 @@ SINGLE_BUTTON = is_enabled(single_button_env)
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '--1001930977544')            
                   
 
-      # URL Shortener #
-
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'api.shareus.in/shortLink')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
-
+      
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 100))
 SELF_DELETE = environ.get('SELF_DELETE', True)
