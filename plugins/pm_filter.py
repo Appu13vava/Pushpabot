c490b72550de30d5c53f8b32632164abeb62a1a7
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2023-10-01 01:25:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -109,7 +109,7 @@ async def next_page(bot, query):
     btn.insert(0,
         [
             InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/+9hpzSIMAO_YyYjU9')
-    InlineKeyboardButton(text="⚡ന്യൂ മൂവീസ് ⚡", url='https://t.me/+9hpzSIMAO_YyYjU9')
+    InlineKeyboardButton(text="⚡new movies ⚡", url='https://t.me/+9hpzSIMAO_YyYjU9')
           ]
     )
 
